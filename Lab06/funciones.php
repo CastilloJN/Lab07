6133ac698d31e0670ffeb5be9a0c45cb3139363e
CastilloJN <102237539+CastilloJN@users.noreply.github.com>
2022-05-11 00:15:41 -0500

    Add files via upload

---
+++ Lab06/funciones.php
@@ -0,0 +1,14 @@
+<?php
+function conectar()
+{
+    $xc = mysqli_connect("localhost", "root", "","lab06");
+    return $xc;
+    die();
+}
+
+function desconectar($xc){
+    mysqli_close($xc);
+}
+
+
+?>
